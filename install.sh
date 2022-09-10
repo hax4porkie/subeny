9e1d56f8c5db45486198cce575ce1bbf18603742
mixtrax <113250689+mixtrax@users.noreply.github.com>
2022-09-10 16:47:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 apt update
-apt install golang httprobe assetfinder curl sublist3r
+apt install golang curl git
+go install github.com/tomnomnom/httprobe@latest
+go get -u github.com/tomnomnom/assetfinder
 curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux-i386.zip
 unzip findomain-linux-i386.zip
 chmod +x findomain
 mv findomain /usr/bin/findomain
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r
+pip install -r requirements.txt
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
 
